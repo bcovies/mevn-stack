c77ad3c09aac47dc6f6b56a81e493058264e1333
Bruno Souto <bcovies@gmail.com>
2022-03-28 22:32:13 -0300

    Removido ejs | adicionado get para parametro

--- backend/node/src/index.js
+++ backend/node/src/index.js
@@ -5,7 +5,14 @@ const serverConfigs = require('./configs/serverConfigs');
 const hostname = serverConfigs.configurations.hostname;
 const port = serverConfigs.configurations.port;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/:id', (req,res) => {
+  res.send(req.params.id);
+});
+
 app.listen(port, () => {
   console.log(`Servidor ${hostname} na porta ${port} Online!`);
 });
