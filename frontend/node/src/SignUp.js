e2d945bd9c76b6c710f8531568ce2d6c90f4c1f0
Bruno Souto <bcovies@gmail.com>
2022-04-04 20:36:25 -0300

    Adicionado react base

---
+++ frontend/node/src/SignUp.js
@@ -0,0 +1,37 @@
+import { Component } from "react";
+
+class NameForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {value: ''};
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  
+  }
+
+  handleNameChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Um nome foi enviado: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Nome:
+          <input type="text" value={this.state.value} onChange={this.handleNameChange} />
+        </label>
+        
+        <input type="submit" value="Enviar" />
+      </form>
+    );
+  }
+}
+
+export default NameForm;
