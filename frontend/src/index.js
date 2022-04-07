9b991a0e98bd02b32c5c9c8546c54d56acdacc47
Bruno Souto <bcovies@gmail.com>
2022-04-06 21:12:08 -0300

    Adicionado roteamento

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Login from './Login';
-import SignUp from './SignUp';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Dashboard from './routes/dashboard'
+import SignUp from './routes/signup'
+import Login from './routes/login'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
-    <SignUp/>
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="signup" element={<SignUp />} />
+        <Route path="login" element={<Login />} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/routes/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Dashboard = () => {
+  return (
+    <p>
+      Página de Dashboard
+    </p>
+  );
+}
+
+export default Dashboard;
