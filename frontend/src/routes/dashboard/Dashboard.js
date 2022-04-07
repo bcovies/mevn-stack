5114a6868d0ee89dd554db3fc7a42a9856fe05d4
Bruno Souto <bcovies@gmail.com>
2022-04-06 21:24:33 -0300

    Adicionado rotas na página principal

--- frontend/src/routes/dashboard/Dashboard.js
+++ frontend/src/routes/dashboard/Dashboard.js
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import {
+  Link
+} from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <p>
-      Página de Dashboard
-    </p>
+    <>
+      <h2>
+        Página de Dashboard
+      </h2>
+      <Link to="/login">Login</Link>
+      <br/>
+      <Link to="/signup">SignUp</Link>
+    </>
   );
 }
 
