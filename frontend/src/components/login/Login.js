6946356910780b910a9280a8c72404cd1cb672de
Bruno Souto <bcovies@gmail.com>
2022-04-06 23:16:37 -0300

    Adicionado login com useState

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,46 +1,83 @@
 import React from 'react';
 import { Button, TextField, Box, Typography } from '@material-ui/core';
 
-const Login = () => {
+class Login extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { 
+      email: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+  }
 
-  return (
-    <>
-      <Box
-        display='flex'
-        flexDirection='row'
-        justifyContent='center'
-        >
-          <Box 
-            display='flex'
-            flexDirection='column'
-            justifyContent='center'
-            >
-              <Typography 
-                variant="h3" component="h3"
-                >
-                  Login
-              </Typography>
-              <TextField
-                id="email"
-                name="email"
-                label="Email"
-                type="email"
-              />
-              <TextField
-                id="password"
-                name="password"
-                label="Senha"
-                type="password"
-              />
-              <Button 
-                variant="contained"
-                >
-                  Login!
-              </Button>
+  handleChangeEmail(event) {    
+    // console.log(event.target.value)
+    this.setState({
+      email: event.target.value
+    });  
+  }
+  
+  handleChangePassword(event) {    
+    // console.log(event.target.value)
+    this.setState({
+      password: event.target.value
+    });  
+  }
+
+  handleSubmit() {
+    alert('Email: ' + this.state.email + ' Senha: ' + this.state.password);
+  }
+
+  render(){
+    return (
+      <>
+      <form onSubmit={this.handleSubmit}>
+        <Box
+          display='flex'
+          flexDirection='row'
+          justifyContent='center'
+          >
+            <Box 
+              display='flex'
+              flexDirection='column'
+              justifyContent='center'
+              >
+                  <Typography 
+                    variant="h3" component="h3"
+                    >
+                      Login
+                  </Typography>
+                  <TextField
+                    id="email"
+                    name="email"
+                    label="Email"
+                    type="email"
+                    // value={this.state.email} 
+                    onChange={this.handleChangeEmail}
+                  />
+                  <TextField
+                    id="password"
+                    name="password"
+                    label="Senha"
+                    type="password"
+                    // value={this.state.password} 
+                    onChange={this.handleChangePassword}
+                  />
+                  <Button 
+                    variant="contained"
+                    type='submit'
+                    >
+                      Login!
+                  </Button>
+            </Box>
           </Box>
-        </Box>
-    </>
-  );
+      </form>
+      </>
+    );
+  }
 }
 
 export default Login;
