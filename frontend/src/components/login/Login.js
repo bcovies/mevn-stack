03095faf655089affbc52bb72514392d21a34c39
Bruno Souto <bcovies@gmail.com>
2022-04-06 22:07:10 -0300

    Adicionado página de login

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Button, TextField, Box } from '@material-ui/core';
 
 const Login = () => {
+
   return (
-    <p>
-        Página de Login
-    </p>
+    <>
+      <h2>Página de Login</h2>
+      <Box
+        display='flex'
+        flexDirection='row'
+        justifyContent='center'
+        >
+          <Box 
+            display='inline-flex'
+            flexDirection='column'
+            >
+              <TextField
+                id="email"
+                name="email"
+                label="Email"
+                type="email"
+              />
+              <TextField
+                id="password"
+                name="password"
+                label="Senha"
+                type="password"
+              />
+              <Button 
+                variant="contained"
+                >
+                  Login!
+              </Button>
+          </Box>
+        </Box>
+    </>
   );
 }
 
