2959a12bba706c7a60bdec20063117730a4b09b2
Bruno Souto <bcovies@gmail.com>
2022-04-08 22:22:21 -0300

    Adicionado registro de usuário melhorado

--- backend/src/controllers/registerController.js
+++ backend/src/controllers/registerController.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-
 const User = require('../models/userSchema');
   
 router.post('/register', async (req,res) => {
+    const { email } = req.body;
     try{
+        if( await User.findOne({ email })){
+            return res.status(400).send({ error: 'User already registred'});
+        }
         const user = await User.create(req.body);
+        user.password = undefined; 
         return res.send({ user });
     } catch (error) {
         return res.status(400).send({
--- backend/src/models/userSchema.js
+++ backend/src/models/userSchema.js
@@ -1,6 +1,7 @@
 //GLOBAL IMPORTS
 const mongoose = require('../database/mongooseConnection');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const userSchema = Schema({
     email: {
@@ -27,6 +28,12 @@ const userSchema = Schema({
         default: Date.now,
     }
 },{colections:'users'});
+
+userSchema.pre('save', async function(next){
+    const hash = await bcrypt.hash(this.password, 1);
+    this.password = hash;
+    next();
+});
 
 const Users = mongoose.model("Users",userSchema);
 
