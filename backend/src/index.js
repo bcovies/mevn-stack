5b7864ff2df3bc07e3df9a85645477c71bb4a761
Bruno Souto <bcovies@gmail.com>
2022-04-10 14:22:09 -0300

    Adicionado rota de checagem de autenticação

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,6 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
 
+require('./controllers/authStatusController')(app);
 require('./controllers/registerController')(app);
 require('./controllers/loginController')(app);
 
