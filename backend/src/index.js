c5403566277e88797bd9b644d35fb3eb07a5f423
Bruno Souto <bcovies@gmail.com>
2022-04-10 21:55:38 -0300

    Reutilizado cors para debug

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,9 +9,9 @@ const port = serverConfigs.port;
 const app = express();
 
 app.use(function (req, res, next) {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  // res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS, HEAD');
-  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   // res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
