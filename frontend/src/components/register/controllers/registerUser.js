e12d97700b54c87d8c5de08125dd3ab61965a24d
Bruno Souto <bcovies@gmail.com>
2022-04-10 21:59:45 -0300

    Adicionado response melhor para frontnend console

--- frontend/src/components/register/controllers/registerUser.js
+++ frontend/src/components/register/controllers/registerUser.js
@@ -4,14 +4,20 @@ module.exports = function(user){
   return new Promise(
     returnPromise => {
       const api_endpoint = process.env.REACT_APP_API_ENDPOINT;
+      console.log(user)
       axios.post(`//${api_endpoint}/auth/register`, user)
       .then(function (response) {
-        console.log(response);
-        returnPromise(response.status);
+        returnPromise({
+          "status": response.status,
+          "email": response.data.user.email,
+          "token": response.data.token
+        });
       })
       .catch(function (error) {
-        console.log(error);
-        returnPromise(error.response.data);
+        returnPromise({
+          "error": error.response.data.error,
+          "status": error.response.status
+        });
       });
     }
   );
