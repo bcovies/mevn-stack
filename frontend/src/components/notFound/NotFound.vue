5fa9a73892ea6c64a1b41c78d04c4e8192aef815
Bruno Souto <bcovies@gmail.com>
2022-05-05 22:29:11 -0300

    Removido react e adicionado Vue

---
+++ frontend/src/components/notFound/NotFound.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>404 Not Found 🙃</h1>
+</template>
--- reactFrontend/src/components/register/controllers/reactRedirect.js
+++
@@ -1,6 +0,0 @@
-function redirectToLogin(){
-    console.log(`${window.location.hostname}`);
-    window.location.replace(`${window.location.hostname}/login`);
-}
-
-module.exports = redirectToLogin;
