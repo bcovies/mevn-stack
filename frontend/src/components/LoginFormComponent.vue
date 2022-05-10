df94178e8953abd8db5f2054044a43ca0fa913b8
Bruno Souto <bcovies@gmail.com>
2022-05-10 20:58:53 -0300

    adicionado login

--- frontend/src/components/LoginFormComponent.vue
+++ frontend/src/components/LoginFormComponent.vue
@@ -5,9 +5,7 @@ export default {
     return {
       email: "",
       password: "",
-      dob: "",
-      phone: "",
-      registerMessage: "",
+      loginMessage: "",
     };
   },
   methods: {
@@ -17,20 +15,12 @@ export default {
     onInputpassword(e) {
       this.password = e.target.value;
     },
-    onInputDob(e) {
-      this.dob = e.target.value;
-    },
-    onInputPhone(e) {
-      this.phone = e.target.value;
-    },
-    register() {
+    getToken() {
       return new Promise((returnPromise) => {
         axios
-          .post(`//${process.env.VUE_APP_API_ENDPOINT}/auth/register`, {
+          .post(`//${process.env.VUE_APP_API_ENDPOINT}/auth/token`, {
             email: this.email,
             password: this.password,
-            dob: this.dob,
-            phone: this.phone,
           })
           .then(function (response) {
             returnPromise({
@@ -46,18 +36,47 @@ export default {
           });
       });
     },
-    async onSubmitRegister() {
+    login(token) {
+      return new Promise((returnPromise) => {
+        axios
+          .post(
+            `//${process.env.VUE_APP_API_ENDPOINT}/auth/token`,
+            {
+              email: this.email,
+              password: this.password,
+            },
+            {
+              headers: {
+                token: `${token}`,
+              },
+            }
+          )
+          .then(function (response) {
+            returnPromise({
+              status: response.status,
+              body: response.data,
+            });
+          })
+          .catch(function (error) {
+            returnPromise({
+              status: error.response.status,
+              body: error.response.data,
+            });
+          });
+      });
+    },
+    async onSubmitLogin() {
       console.log(
         `Sending to backend (${process.env.VUE_APP_API_ENDPOINT}) login informations...`
       );
       console.log(`${this.email}:${this.password}:${this.dob}:${this.phone}`);
-      const data = await this.register();
+      const data = await this.getToken();
 
       if (data.status != "200") {
         // alert("There was an error:" + data.body.error);
-        this.registerMessage = data.body.error;
+        this.loginMessage = data.body.error;
       } else {
-        this.registerMessage = "User registred with success!";
+        const data = await this.login(data.body.token);
       }
     },
   },
@@ -66,9 +85,9 @@ export default {
 
 <template>
   <div>
-    <h1>Register page</h1>
-    <h2>{{ registerMessage }}</h2>
-    <form @submit.prevent="onSubmitRegister">
+    <h1>Login page</h1>
+    <h2>{{ loginMessage }}</h2>
+    <form @submit.prevent="onSubmitLogin">
       <input
         :value="email"
         @input="onInputEmail"
@@ -83,21 +102,7 @@ export default {
         placeholder="Password"
       />
       <br />
-      <input
-        :value="dob"
-        @input="onInputDob"
-        type="text"
-        placeholder="Day of Birthday"
-      />
-      <br />
-      <input
-        :value="phone"
-        @input="onInputPhone"
-        type="text"
-        placeholder="Phone"
-      />
-      <br />
-      <button type="submit">Register</button>
+      <button type="submit">Login</button>
     </form>
   </div>
 </template>
--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>
-    <h1>Login template</h1>
-  </div>
+  <LoginFormComponent />
 </template>
+
+<script>
+// @ is an alias to /src
+import LoginFormComponent from "@/components/LoginFormComponent.vue";
+
+export default {
+  //Nome da classe exportada, no caso AboutView.vue
+  name: "RegisterView",
+  components: {
+    //Componentes que nela estão sendo utilizadas, neste caso, somente About.
+    LoginFormComponent,
+  },
+};
+</script>
