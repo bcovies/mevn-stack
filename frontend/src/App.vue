649093e711432ed94eb99bc543fdc63eb307eeee
Bruno Souto <bcovies@gmail.com>
2022-05-18 19:01:16 -0300

    Adicionado onMount()

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,45 @@
-<script setup>
-import { ref } from "vue";
-const isLoged = ref(true);
-console.log(isLoged);
+<script>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+async function checkUserLogged() {
+  return new Promise((returnPromise) => {
+    axios
+      .get(`//${process.env.VUE_APP_API_ENDPOINT}/auth/test`)
+      .then(function (response) {
+        console.log(response);
+        returnPromise({
+          status: response.status,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+        returnPromise({
+          status: error.response.status,
+        });
+      });
+  });
+}
+export default {
+  setup() {
+    const isLoged = ref(false);
+    onMounted(async (isLoged) => {
+      const userLoggedVar = await checkUserLogged();
+      console.log(userLoggedVar);
+      let aux = false;
+      if (userLoggedVar.status == 200) {
+        console.log("true");
+        aux = true;
+      } else {
+        console.log("false");
+        aux = false;
+      }
+      console.log(aux);
+      isLoged = ref(aux);
+      return isLoged;
+    });
+    return { isLoged };
+  },
+};
 </script>
 
 <template>
@@ -9,6 +47,8 @@ console.log(isLoged);
     <div v-if="isLoged">
       <router-link to="/dashboard">dashboard</router-link> |
       <router-link to="/logout">logout</router-link>
+      <router-link to="/login">Login</router-link> |
+      <router-link to="/register">Regiser</router-link>
     </div>
     <div v-else>
       <router-link to="/login">Login</router-link> |
